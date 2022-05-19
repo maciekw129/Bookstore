602c39c15c009cb65ee6efbffed99e577629abe4
Maciej Walecki <maciejwalecki99@gmail.com>
2022-05-19 09:28:28 +0200

    added book slider

--- force-app/main/default/lwc/homepageBooks/homepageBooks.js
+++ force-app/main/default/lwc/homepageBooks/homepageBooks.js
@@ -3,10 +3,36 @@ import { LightningElement, api } from 'lwc';
 export default class HomepageBooks extends LightningElement {
     @api title;
     @api books;
+    page = 0;
 
-    connectedCallback() {
-        console.log('books: ' + this.books.data)
-        console.log('title: ' + this.title)
+    get numberOfPages() {
+        return this.books.length / 3 - 1;
+    }
+
+    get translateCounter() {
+        return `translateX(-${this.page * 300}%)`;
+    }
+
+    translate() {
+        this.template.querySelectorAll(".books__book").forEach(item => {
+            Object.assign( item.style, {transform: this.translateCounter});
+        });
+    }
+
+    handleLeftClick() {
+        if(this.page <= 0) {
+            return
+        }
+        this.page -= 1;
+        this.translate();
+    }
+
+    handleRightClick() {
+        if(this.page >= this.numberOfPages) {
+            return
+        }
+        this.page += 1;
+        this.translate();
     }
 
 }
